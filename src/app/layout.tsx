c3b910669d435d627c9edefe1ee18c79d93a023c
beduldjakurra <beduldjakurra@gmail.com>
2025-09-28 20:46:43 +0700

    Update layout.tsx with new RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <head />
+      <body className="app-body">{children}</body>
     </html>
   );
 }
